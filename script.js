4b2c8a6fa8cec5b979d8bea479ccc94d5445bbb3
Rxckhar <russaf007@mail.ru>
2021-02-06 10:52:49 +0500

    Save

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(document).ready(function() {
+
+    var arrayAds = Array();
+
+
+
     $('#modal').hide();
 
 $("#add").click(function() {
@@ -9,6 +14,55 @@ $("#close").click(function() {
     $('#modal').hide();
 });
 
+$("#newblock").click(function() {
+    var ad = {
+        text: $('textarea[name="text"]').val(),
+        contactName: $('input[name="name"]').val(),
+        contactPhone: $('input[name="phone"]').val()
+    }
 
+    arrayAds.push(ad);
+    console.log(arrayAds);
+
+    renderAds();
+
+
+    
+    
+    $('#modal').hide();
+
+    $('textarea[name="text"]').val('');
+    $('input[name="name"]').val('');
+    $('input[name="phone"]').val('');
+
+    });
+
+
+    
+    //Функция отрисовки новых объявлений
+    function renderAds() {
+        //Очищаем объявление
+        $('#content').html('');
+
+        //Отрисовка объявления
+        arrayAds.map(ad => {
+            var adhtml =
+            '<div class="Content__box" id="box" >' +
+                '<div class="Box__info" id="info">'+ ad.text +'</div>' +
+                '<div class="Box__name" id="name">'+ ad.contactName +'</div>' +
+                '<div class="Box__phone" id="phone">'+ ad.contactPhone +'</div>' +
+            '</div>';
+    
+            $('#content').append(adhtml);
+        });
+
+
+
+        
+    }
 
 });
+
+
+
+
